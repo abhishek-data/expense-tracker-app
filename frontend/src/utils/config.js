ac28f41ba8a74feca6a817d94446a03574df32f7
Abhishek Kumar <abhishekkr1915@gmail.com>
2023-11-26 13:10:57 +0530

    update

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,5 @@
 // export const API_URL = import.meta.env.VITE_REACT_APP_URL
-export const API_URL = "http://54.221.164.13:5000"
+export const API_URL = "http://54.221.164.13:80"
 export function decodeToken(token) {
   const parts = token.split('.');
   const payload = parts[1];
